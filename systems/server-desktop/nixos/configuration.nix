68da091340e6b069490437338964e4f35e3d2bab
Kerb Byqvist <cyrbygh@users.noreply.github.com>
2026-07-10 21:22:11 -0700

    extra stuff to make libinput work

--- systems/server-desktop/nixos/configuration.nix
+++ systems/server-desktop/nixos/configuration.nix
@@ -139,6 +139,8 @@ in
     extraSessionCommands = ''
       export WLR_BACKENDS=headless,libinput
       export WLR_RENDER_DRM_DEVICE=/dev/dri/renderD128
+      export LIBSEAT_BACKEND=noop
+      export WLR_LIBINPUT_NO_DEVICES=1
       export PATH=/run/current-system/sw/bin:/run/wrappers/bin:$PATH
     '';
   };
