7f12dbefd153f343e9972080cded3e8889c1151a
Kerb Byqvist <cyrbygh@users.noreply.github.com>
2026-07-16 22:53:29 -0700

    office-thin-client: finish out config

--- systems/office-thin-client/setup.sh
+++ systems/office-thin-client/setup.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 pushd ../_global > /dev/null
 . setup.sh
 popd > /dev/null
+. ../../scripts/encrypt.sh
 
 link ../_shared/env/nixos ~/.env/nixos
 link ../_shared/gitconfig ~/.gitconfig
+
+crypt-link ./ssh/id_ed25519 ~/.ssh/id_ed25519
+link ./ssh/id_ed25519.pub ~/.ssh/id_ed25519.pub
+
