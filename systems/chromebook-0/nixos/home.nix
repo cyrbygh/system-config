5683feb82f4f9c682b5dd0e0c1c3a6f9ca5a9d75
Kerb Byqvist <cyrbygh@users.noreply.github.com>
2026-07-28 14:23:57 -0700

    bugfix: remove one too many ..

--- systems/chromebook-0/nixos/home.nix
+++ systems/chromebook-0/nixos/home.nix
@@ -4,8 +4,8 @@
   home.stateVersion = "26.05";
 
   imports = [
-    ../../../_shared/home/base.nix
-    ../../../_shared/home/nixos.nix
+    ../../_shared/home/base.nix
+    ../../_shared/home/nixos.nix
   ];
 
   home.file.".ssh/id_ed25519.pub".source = ../ssh/id_ed25519.pub;
