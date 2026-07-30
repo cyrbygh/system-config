581e417ee4d9e0b0ea705b886d6c3239c5f6e615
Kerb Byqvist <cyrbygh@users.noreply.github.com>
2026-07-30 13:15:21 -0700

    allow the out of tree unfree driver for wifi

--- systems/backup-0/nixos/configuration.nix
+++ systems/backup-0/nixos/configuration.nix
@@ -8,7 +8,8 @@
 
   networking.hostName = "backup-0";
 
-  # BCM4360 needs the out-of-tree wl driver from broadcom_sta.
+  # BCM4360 needs the out-of-tree wl driver from broadcom_sta (unfree).
+  nixpkgs.config.allowUnfreePredicate = pkg: lib.getName pkg == "broadcom-sta";
   boot.kernelModules = [ "wl" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
 
